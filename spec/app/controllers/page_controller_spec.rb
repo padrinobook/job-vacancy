a99a9903898d54213102917e38bf66a68b995988
Matthias Guenther <matthias.guenther@wikimatze.de>
2012-12-20 22:48:36 +0100

    Refactored controller page tests

--- spec/app/controllers/page_controller_spec.rb
+++ spec/app/controllers/page_controller_spec.rb
@@ -2,26 +2,12 @@ require 'spec_helper'
 
 describe "PageController" do
 
-  describe "GET #about" do
-
-    it "renders the :about view" do
-      get '/about'
-      last_response.should be_ok
-    end
-  end
-
-  describe "GET #contact" do
-
-    it "renders the :contact view" do
-      get '/contact'
-      last_response.should be_ok
-    end
-  end
-
-  describe "GET #home" do
-    it "renders :home view" do
-      get '/'
-      last_response.should be_ok
+  %w(/ /about /contact).each do |path|
+    describe "GET #{path}" do
+      it "render the :#{path} view" do
+        get path
+        last_response.should be_ok
+      end
     end
   end
 
