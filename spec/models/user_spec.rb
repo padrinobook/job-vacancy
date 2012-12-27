8f8f9ab0f34273e6e62e6f516798c81e5bd761f2
Matthias Guenther <matthias.guenther@wikimatze.de>
2012-12-27 19:50:11 +0100

    New test methods for association between user and a job-offer

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
 describe "User Model" do
-  let(:user) { User.new }
+  let(:user) { FactoryGirl.build(:user) }
+  let(:job_offer) { {:title => 'Padrino Engineer 2', :location => 'Berlin', :description => 'Come to this great place'} }
   it 'can be created' do
     user.should_not be_nil
   end
+
+  it 'fresh user should have no offers' do
+    user.job_offers.size.should == 0
+  end
+
+  it 'have job-offers' do
+    user.job_offers.build(job_offer)
+    user.job_offers.size.should == 1
+  end
+
 end
