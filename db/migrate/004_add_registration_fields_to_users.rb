d1ba1272f2ae73137816d8f3245641fba19d1ca0
Matthias Günther <matthias.guenther@wikimatze.de>
2013-03-20 08:11:12 +0100

    Add database migration for user registration

---
+++ db/migrate/004_add_registration_fields_to_users.rb
@@ -0,0 +1,16 @@
+class AddRegistrationFieldsToUsers < ActiveRecord::Migration
+
+  @fields = [:password, :password_confirmation]
+
+  def self.up
+    change_table :users do |t|
+      @fields.each { |field| t.string field}
+    end
+  end
+
+  def self.down
+    change_table :users do |t|
+      @fields.each { |field| remove_column field}
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 3) do
+ActiveRecord::Schema.define(:version => 4) do
 
   create_table "job_offers", :force => true do |t|
     t.string   "title"
@@ -28,8 +28,10 @@ ActiveRecord::Schema.define(:version => 3) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
+    t.string   "password"
+    t.string   "password_confirmation"
   end
 
 end
