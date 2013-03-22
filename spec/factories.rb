6c3ea895a8f1117db3752f885ffa600d5d7c59cc
Matthias Günther <matthias.guenther@wikimatze.de>
2013-03-22 05:26:48 +0100

    Factory with sequence to guarantee the :build method to create fresh users

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,18 +1,21 @@
 # encoding: utf-8
 FactoryGirl.define do |u|
+  sequence(:email){ |n| "matthias.guenther#{n}@wikimatze.de"}
 
   factory :user do
-    name  " Matthias Günther               "
-    email " matthias.guenther@wikimatze.de "
+    name  "Matthias Günther"
+    email
+    password "foo"
+    password_confirmation "foo"
   end
 
   factory :job_offer do
-    title       " Padrino Engineer    "
-    location    " Berlin              "
-    description " We want you ...     "
-    contact     " recruter@awesome.de "
-    time_start  " 1/01/2013           "
-    time_end    " 01/03/2013          "
+    title       "Padrino Engineer   "
+    location    "Berlin             "
+    description "We want you ...    "
+    contact     "recruter@awesome.de"
+    time_start  "1/01/2013          "
+    time_end    "01/03/2013         "
   end
 
 end
