f673d8d305d649fdfb7a7b19de8523486950f02c
Matthias Günther <matthias.guenther@wikimatze.de>
2013-03-23 18:23:51 +0100

    - Validation of not blank for name, email, and password - name and email should be unique in whole application

--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, :email, :password, :presence => true
+  validates :name, :uniqueness => true
+  validates :email, :uniqueness => true
+
   has_many :job_offers
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,40 @@ describe "User Model" do
     user.job_offers.size.should == 1
   end
 
+  it 'have no blank name' do
+    user.name = ""
+    user.save.should be_false
+  end
+
+  it 'have no blank email' do
+    user.email = ""
+    user.save.should be_false
+  end
+
+  it 'have no blank password' do
+    user.password = ""
+    user.save.should be_false
+  end
+
+  describe "when name is already used" do
+    let(:user_second) { build(:user) }
+
+    it 'should not be saved' do
+       user_second.save.should be_false
+    end
+  end
+
+  describe "when email adress is already used" do
+    let(:user_second) { build(:user) }
+
+    it 'should not save an user with an existing adress' do
+      user_second.name = "Hansi"
+      user_second.email = user.email
+      user_second.save.should be_false
+    end
+  end
+
+  pending('have a valid email')
+  pending('the password should have a min lenght of 5 characters')
+  pending('the password and password confirmation should be equal')
 end
