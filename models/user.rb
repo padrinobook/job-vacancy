2feda47892c5271fcc389f66f24c99eb51f29b8a
Matthias Günther <matthias.guenther@wikimatze.de>
2013-04-10 22:20:46 +0200

    - Add virtual confirmation attribut for password_confirmation - controller spec for login page

--- models/user.rb
+++ models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
-  validates :name, :presence => true, :uniqueness => true
-  validates :password, :length => {:minimum => 5}, :presence => true
+  validates :name, :presence => true,
+                   :uniqueness => true
+
+  validates :password, :length => {:minimum => 5},
+                       :presence => true,
+                       :confirmation => true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, :presence => true,
--- spec/app/controllers/users_controller_spec.rb
+++ spec/app/controllers/users_controller_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe "UsersController" do
+
+  %w(/login).each do |path|
+    describe "GET #{path}" do
+      it "render the :#{path} view" do
+        get path
+        last_response.should be_ok
+      end
+    end
+  end
 end
