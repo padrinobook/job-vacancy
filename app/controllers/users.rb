6feacb35dcd500697c62f0d6e8bc70e03275f95a
Matthias Günther <matthias.guenther@wikimatze.de>
2013-04-10 22:10:13 +0200

    Add form for new registration + including error messages

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,7 +1,16 @@
 JobVacancy.controllers :users do
 
   get :new, :map => "/login" do
+    @user = User.new
     render 'users/new'
   end
 
+  post :create do
+    @user = User.new(params[:user])
+    if @user.save
+      redirect('/')
+    else
+      render 'users/new'
+    end
+  end
 end
