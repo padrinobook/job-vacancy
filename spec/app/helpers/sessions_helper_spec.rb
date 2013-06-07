4cee491008f152938f49e312b6ac921abf8658df
Matthias Günther <matthias.guenther@wikimatze.de>
2013-06-07 18:32:24 +0200

    Implement the rest of the session helper spec

--- spec/app/helpers/sessions_helper_spec.rb
+++ spec/app/helpers/sessions_helper_spec.rb
@@ -12,7 +12,7 @@ describe SessionsHelper do
     @session_helper = SessionsHelperClass.new
   end
 
-  context "current_user" do
+  context "#current_user" do
     it "output the current user if current user is already set" do
       user = User.new
       @session_helper.current_user = user
@@ -25,29 +25,44 @@ describe SessionsHelper do
       browser = Rack::Test::Session.new(Rack::MockSession.new(Sinatra::Application))
       browser.get '/', {}, 'rack.session' => { :current_user => user.id }
       @session_helper.should_receive(:last_request).and_return(browser.last_request)
-
       @session_helper.current_user.should == user
-
     end
   end
 
-  context "current_user?" do
-    it "returns true if current user is logged in"
-    it "returns false if user is not logged in"
+  context "#current_user?" do
+    it "returns true if current user is logged in" do
+      user = User.new
+      @session_helper.should_receive(:current_user).and_return(user)
+      @session_helper.current_user?(user).should be_true
+    end
+
+    it "returns false if user is not logged in" do
+      user = User.new
+      @session_helper.should_receive(:current_user).and_return(nil)
+      @session_helper.current_user?(user).should be_false
+    end
   end
 
-  context " sign_in" do
-    it "creates a session"
-  end
-
-  context " sign_out" do
-    it "destroys the session"
+  context "#sign_in" do
+    it "it sets the current user to the signed in user" do
+      user = User.first
+      browser = Rack::Test::Session.new(Rack::MockSession.new(Sinatra::Application))
+      browser.get '/', {}, 'rack.session' => { :current_user => user.id }
+      @session_helper.should_receive(:session).and_return(browser.last_request)
+      @session_helper.sign_in(user)
+      @session_helper.current_user.should == user
+    end
   end
 
   context "#signed_in?" do
     it "return false if user is not logged in" do
+      @session_helper.should_receive(:current_user).and_return(nil)
+      @session_helper.signed_in?.should be_false
     end
 
-    it "return true if user is logged in"
+    it "return true if user is logged in" do
+      @session_helper.should_receive(:current_user).and_return(User.new)
+      @session_helper.signed_in?.should be_true
+    end
   end
 end
