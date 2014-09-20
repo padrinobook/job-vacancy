45d73042e015de2aa882ef15ba884b391ade291a
Matthias Guenther <matthias@wikimatze.de>
2014-09-20 14:21:42 +0200

    Add migration for user password reset

---
+++ db/migrate/007_add_password_reset_token_to_users.rb
@@ -0,0 +1,15 @@
+class AddPasswordResetTokenToUsers < ActiveRecord::Migration
+  def self.up
+    change_table :users do |t|
+      t.string :password_reset_token, default: 0, null: true
+      t.datetime :password_reset_sent_date, default: 0, null: true
+    end
+  end
+
+  def self.down
+    change_table :users do |t|
+      t.remove :password_reset_token
+      t.remove :password_reset_sent_date
+    end
+  end
+end
