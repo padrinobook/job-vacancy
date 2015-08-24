c24f0a858716bd2de008414f1eca3116edd26dea
Matthias Guenther <matthias@wikimatze.de>
2015-08-24 20:57:38 +0200

    Expect, to_falsey to_truthy syntax

--- spec/app/helpers/sessions_helper_spec.rb
+++ spec/app/helpers/sessions_helper_spec.rb
@@ -13,7 +13,7 @@ describe SessionsHelper do
     it "output the current user if current user is already set" do
       user = User.new
       @session_helper.current_user = user
-      @session_helper.current_user.should == user
+      expect(@session_helper.current_user).to eq user
     end
 
     it "find the user by id from the current session" do
@@ -21,7 +21,7 @@ describe SessionsHelper do
       browser = Rack::Test::Session.new(JobVacancy::App)
       browser.get '/', {}, 'rack.session' => { :current_user => user.id }
       @session_helper.should_receive(:last_request).and_return(browser.last_request)
-      @session_helper.current_user.should == user
+      expect(@session_helper.current_user).to eq user
     end
   end
 
@@ -29,13 +29,13 @@ describe SessionsHelper do
     it "returns true if current user is logged in" do
       user = User.new
       @session_helper.should_receive(:current_user).and_return(user)
-      @session_helper.current_user?(user).should be_true
+      expect(@session_helper.current_user?(user)).to be_truthy
     end
 
     it "returns false if user is not logged in" do
       user = User.new
       @session_helper.should_receive(:current_user).and_return(nil)
-      @session_helper.current_user?(user).should be_false
+      expect(@session_helper.current_user?(user)).to be_falsey
     end
   end
 
@@ -46,19 +46,19 @@ describe SessionsHelper do
       browser.get '/', {}, 'rack.session' => { :current_user => user.id }
       @session_helper.should_receive(:session).and_return(browser.last_request)
       @session_helper.sign_in(user)
-      @session_helper.current_user.should == user
+      expect(@session_helper.current_user).to eq user
     end
   end
 
   context "#signed_in?" do
     it "return false if user is not logged in" do
       @session_helper.should_receive(:current_user).and_return(nil)
-      @session_helper.signed_in?.should be_false
+      expect(@session_helper.signed_in?).to be_falsey
     end
 
     it "return true if user is logged in" do
       @session_helper.should_receive(:current_user).and_return(User.new)
-      @session_helper.signed_in?.should be_true
+      expect(@session_helper.signed_in?).to be_truthy
     end
   end
 end
