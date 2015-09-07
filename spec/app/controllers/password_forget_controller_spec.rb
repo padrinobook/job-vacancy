1b738ab0405695373540ad5c638900a9db07d937
Matthias Guenther <matthias@wikimatze.de>
2015-09-07 17:42:38 +0200

    Test get action from password forget controller

--- spec/app/controllers/password_forget_controller_spec.rb
+++ spec/app/controllers/password_forget_controller_spec.rb
@@ -1,13 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe "PasswordForgetController" do
-  pending "add some examples to #{__FILE__}" do
-    before do
-      get "/"
-    end
-
-    it "returns hello world" do
-      expect(last_response.body).to eq "Hello World"
+  describe "GET password_forget" do
+    it "renders new page" do
+      get 'password_forget'
+      expect(last_response).to be_ok
+      expect(last_response.body).to include 'Forget Password'
     end
   end
 end
