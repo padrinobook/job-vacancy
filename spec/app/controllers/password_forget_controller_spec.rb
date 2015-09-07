be61804cd1e09fc8b13a118d42a93b9b24c9781c
Matthias Guenther <matthias@wikimatze.de>
2015-09-07 18:19:19 +0200

    Add first passwort forget create action test

--- spec/app/controllers/password_forget_controller_spec.rb
+++ spec/app/controllers/password_forget_controller_spec.rb
@@ -8,5 +8,14 @@ RSpec.describe "PasswordForgetController" do
       expect(last_response.body).to include 'Forget Password'
     end
   end
+
+  describe "POST create" do
+    it "renders success even if user was not found" do
+      expect(User).to receive(:find_by_email).and_return(nil)
+      post '/password_forget/create'
+      expect(last_response).to be_ok
+      expect(last_response.body).to include 'Password was reseted successfully'
+    end
+  end
 end
 
