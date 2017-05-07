71e2e8e6b964b374486481e7ef28af967e97c469
Matthias Guenther <matthias@wikimatze.de>
2017-05-07 10:44:03 +0200

    user_spec is now fixed with rubocop + rename user_confirmation wirh confirmation_user

--- spec/app/models/user_spec.rb
+++ spec/app/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe "User Model" do
   let(:user) { build_stubbed(:user) }
-  let(:user_second) { build_stubbed(:user)}
+  let(:user_second) { build_stubbed(:user) }
 
   it 'can be created' do
     expect(user).not_to be_nil
@@ -12,35 +12,35 @@ RSpec.describe "User Model" do
     expect(user.job_offers.size).to eq 0
   end
 
-  it 'have job-offers' do
+  it 'has job-offers' do
     user.job_offers.build(attributes_for(:job_offer))
     expect(user.job_offers.size).to eq 1
   end
 
-  it 'no blank name' do
-    user.name = ""
+  it 'has no blank name' do
+    user.name = ''
     expect(user.valid?).to be_falsey
   end
 
-  it 'no blank email' do
-    user.email = ""
+  it 'has no blank email' do
+    user.email = ''
     expect(user.valid?).to be_falsey
   end
 
   describe "passwords" do
-    let(:user_confirmation) { build(:user)}
+    let(:user_confirmation) { build(:user) }
     it 'no blank password' do
-      user_confirmation.password = ""
+      user_confirmation.password = ''
       expect(user_confirmation.valid?).to be_falsey
     end
 
     it 'password length should be at least 5 characters long' do
-      user_confirmation.password = "octo"
+      user_confirmation.password = 'octo'
       expect(user_confirmation.valid?).to be_falsey
     end
   end
 
-  describe "when name is already used" do
+  describe "name is already used" do
     it 'should not be saved' do
       User.destroy_all
       user = build(:user)
@@ -62,12 +62,12 @@ RSpec.describe "User Model" do
     end
   end
 
-  describe "email address" do
+  describe 'email address' do
     it 'valid' do
       adresses = %w[thor@marvel.de hero@movie.com]
       adresses.each do |email|
         user.email = email
-        user_second.email= email
+        user_second.email = email
         expect(user_second.valid?).to be_truthy
       end
     end
@@ -75,30 +75,30 @@ RSpec.describe "User Model" do
     it 'not valid' do
       adresses = %w[spamspamspam.de heman,test.com]
       adresses.each do |email|
-        user_second.email= email
+        user_second.email = email
         expect(user_second.valid?).to be_falsey
       end
     end
   end
 
-  describe "confirmation code" do
-    let(:user_confirmation) { build(:user) }
+  describe 'confirmation code' do
+    let(:confirmation_user) { build(:user) }
 
     it 'should authenticate user with correct confirmation code and should be confirmed' do
-      user_confirmation.save
-      confirmation_of_saved_user = User.find_by_id(user_confirmation.id)
-      user_confirmation.confirmation_code = confirmation_of_saved_user.confirmation_code
-      expect(user_confirmation.authenticate(user_confirmation.confirmation_code)).to be_truthy
-      expect(user_confirmation.confirmation).to be_truthy
+      confirmation_user.save
+      confirmation_of_saved_user = User.find_by_id(confirmation_user.id)
+      confirmation_user.confirmation_code = confirmation_of_saved_user.confirmation_code
+      expect(confirmation_user.authenticate(confirmation_user.confirmation_code)).to be_truthy
+      expect(confirmation_user.confirmation).to be_truthy
     end
 
     it 'should not authenticate user with incorrect confirmation code' do
-      expect(user_confirmation.authenticate("wrong")).to be_falsey
+      expect(confirmation_user.authenticate('wrong')).to be_falsey
     end
   end
 
   describe "#generate_authentity_token" do
-    let(:user_confirmation) { build(:user) }
+    let(:confirmation_user) { build(:user) }
 
     it 'generates the authentity_token before user is saved' do
       expect(user).to receive(:save).and_return(true)
@@ -108,3 +108,4 @@ RSpec.describe "User Model" do
     end
   end
 end
+
