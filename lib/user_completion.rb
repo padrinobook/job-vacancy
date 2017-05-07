3438eb56e039778d4dc2f180bb89ba8fe0dd6ed0
Matthias Guenther <matthias@wikimatze.de>
2017-05-07 19:09:02 +0200

    Get rid of self in user-completion lib + spec cleanup

--- lib/user_completion.rb
+++ lib/user_completion.rb
@@ -10,11 +10,11 @@ class UserCompletion
   end
 
   def send_registration_mail
-    self.app.deliver(:registration, :registration_email, self.user.name, self.user.email)
+    app.deliver(:registration, :registration_email, user.name, user.email)
   end
 
   def send_confirmation_mail
-    self.app.deliver(:confirmation, :confirmation_email, user.name,
+    app.deliver(:confirmation, :confirmation_email, user.name,
       user.email,
       user.id,
       user.confirmation_code)
@@ -22,7 +22,8 @@ class UserCompletion
 
   def encrypt_confirmation_code
     salt = BCrypt::Engine.generate_salt
-    confirmation_code = BCrypt::Engine.hash_secret(self.user.password, salt)
-    self.user.confirmation_code = normalize(confirmation_code)
+    confirmation_code = BCrypt::Engine.hash_secret(user.password, salt)
+    user.confirmation_code = normalize(confirmation_code)
   end
 end
+
