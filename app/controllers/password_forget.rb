93418dcb9c9d3fe0438cdb2772478dcd66356b34
Matthias Guenther <matthias@wikimatze.de>
2017-05-14 20:13:54 +0200

    Unified password_forget routes and make the specs more readable

--- app/controllers/password_forget.rb
+++ app/controllers/password_forget.rb
@@ -8,32 +8,32 @@ JobVacancy::App.controllers :password_forget do
 
     if @user
       @user.save_forget_password_token
-      link = "http://localhost:3000" + url(:password_forget, :edit, :token => @user.password_reset_token)
+      link = 'http://localhost:3000' + url(:password_forget, :edit, token: @user.password_reset_token)
       deliver(:password_reset, :password_reset_email, @user, link)
     end
 
     render 'success'
   end
 
-  get :edit, :map => "/password-reset/:token/edit" do
+  get :edit, :map => "/password_forget/:token/edit" do
     @user = User.find_by_password_reset_token(params[:token])
 
-    if @user && Time.now.to_datetime < (@user.password_reset_sent_date.to_datetime + (1.0/24.0))
+    if @user && Time.now < (@user.password_reset_sent_date + (1.0 / 24.0))
       render 'edit'
-    elsif @user && Time.now.to_datetime >= (@user.password_reset_sent_date.to_datetime + (1.0/24.0))
-      @user.update_attributes({:password_reset_token => 0, :password_reset_sent_date => 0})
+    elsif @user && Time.now >= (@user.password_reset_sent_date + (1.0 / 24.0))
+      @user.update_attributes(password_reset_token: 0, password_reset_sent_date: 0)
       redirect url(:sessions, :new), flash[:error] = 'Password reset token has expired.'
     else
       redirect url(:password_forget, :new)
     end
   end
 
-  post :update, :map => "password-reset/:token" do
+  post :update, :map => "password_forget/:token" do
     @user = User.find_by_password_reset_token(params[:token])
 
     if @user && @user.update_attributes(params[:user])
-      @user.update_attributes({:password_reset_token => 0, :password_reset_sent_date => 0})
-      redirect url(:sessions, :new), flash[:notice] = "Password has been reseted. Please login with your new password."
+      @user.update_attributes(password_reset_token: 0, password_reset_sent_date: 0)
+      redirect url(:sessions, :new), flash[:notice] = 'Password has been reseted. Please login with your new password.'
     else
       render 'edit'
     end
--- spec/app/controllers/password_forget_controller_spec.rb
+++ spec/app/controllers/password_forget_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe "PasswordForgetController" do
   describe "GET /password_forget" do
-    it "renders new page" do
+    it 'renders new page' do
       get 'password_forget'
       expect(last_response).to be_ok
       expect(last_response.body).to include 'Forget Password'
@@ -10,16 +10,16 @@ RSpec.describe "PasswordForgetController" do
   end
 
   describe "POST /password_forget/create" do
-    let(:user) { build(:user)}
+    let(:user) { build(:user) }
 
-    it "renders success even if user was not found" do
+    it 'renders success even if user was not found' do
       expect(User).to receive(:find_by_email).and_return(nil)
       post '/password_forget/create'
       expect(last_response).to be_ok
       expect(last_response.body).to include 'Password was reseted successfully'
     end
 
-    it "renders success even if user was found" do
+    it 'renders success even if user was found' do
       expect(User).to receive(:find_by_email).and_return(user)
       post '/password_forget/create'
       expect(last_response).to be_ok
@@ -27,46 +27,48 @@ RSpec.describe "PasswordForgetController" do
     end
   end
 
-  describe "GET /password-reset/:token/edit" do
-    let(:user) { build_stubbed(:user)}
+  describe "GET /password_forget/:token/edit" do
+    let(:user) { build_stubbed(:user) }
 
-    it "renders edit page from password-forget" do
-      test_time = Time.local(2015, "sep", 9, 7, 00, 00, 00).to_datetime
-      expect(Time).to receive(:now).and_return(test_time,test_time,test_time,test_time,test_time,test_time,test_time)
+    it 'renders edit page from password-forget' do
+      test_time = Time.now
+      allow(Time).to receive(:now).and_return(test_time)
 
-      user.password_reset_sent_date = test_time + 1.0/24.0
+      user.password_reset_sent_date = test_time + 1.0 / 24.0
       expect(User).to receive(:find_by_password_reset_token).and_return(user)
-      get '/password-reset/1/edit'
+      get '/password_forget/1/edit'
       expect(last_response).to be_ok
       expect(last_response.body).to include 'Reset Password'
     end
 
-    it "redirects to new session because password reset timestamp was over one hour ago" do
-      test_time = Time.local(2015, "sep", 9, 7, 00, 00, 00).to_datetime
-      expect(Time).to receive(:now).and_return(test_time,test_time,test_time)
+    it 'redirects to new session because password reset timestamp was over one hour ago' do
+      test_time = Time.now
+      allow(Time).to receive(:now).and_return(test_time)
 
-      user.password_reset_sent_date = test_time - 1.0/24.0
+      user.password_reset_sent_date = test_time - 1.0 / 24.0
       expect(User).to receive(:find_by_password_reset_token).and_return(user)
-      expect(user).to receive(:update_attributes)
-      get '/password-reset/1/edit'
+      expect(user).to receive(:update_attributes).with({ password_reset_token: 0, password_reset_sent_date: 0 })
+
+      get '/password_forget/1/edit'
+
       expect(last_response).to be_redirect
       expect(last_response.body).to include 'Password reset token has expired.'
     end
 
-    it "redirects to /password_forget if user is not found" do
+    it 'redirects to /password_forget if user is not found' do
       expect(User).to receive(:find_by_password_reset_token).and_return(nil)
-      get '/password-reset/1/edit'
+      get '/password_forget/1/edit'
       expect(last_response).to be_redirect
     end
   end
 
-  describe "POST /password-reset/:token" do
+  describe "POST /password_forget/:token" do
     let(:user) { build_stubbed(:user) }
 
     it "renders edit page if user can be updated" do
       expect(User).to receive(:find_by_password_reset_token).and_return(user)
       expect(user).to receive(:update_attributes).and_return(true, true)
-      post '/password-reset/1'
+      post '/password_forget/1'
       expect(last_response).to be_redirect
       expect(last_response.body).to include 'Password has been reseted. Please login with your new password.'
     end
@@ -74,7 +76,7 @@ RSpec.describe "PasswordForgetController" do
     it "renders edit page if user can not be updated" do
       expect(User).to receive(:find_by_password_reset_token).and_return(user)
       expect(user).to receive(:update_attributes).and_return(false)
-      post '/password-reset/1'
+      post '/password_forget/1'
       expect(last_response).to be_ok
     end
   end
