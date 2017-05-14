295cee82ac418455d8ed441c779adb23e5d03bc0
Matthias Guenther <matthias@wikimatze.de>
2017-05-14 08:14:51 +0200

    variable for magic value in session-controller spec

--- spec/app/controllers/sessions_controller_spec.rb
+++ spec/app/controllers/sessions_controller_spec.rb
@@ -37,28 +37,29 @@ RSpec.describe "SessionsController" do
       user.confirmation = true
       user.password = 'correct'
       expect(User).to receive(:find_by_email) { user }
-      post 'sessions/create', password: 'correct', remember_me: false
+      post 'sessions/create', password: 'correct'
       expect(last_response).to be_redirect
     end
 
     it 'redirects if user is correct and has remember_me' do
       token = 'real'
       user = double('User')
+      thirty_days_in_seconds = 2592000
       expect(user).to receive(:id) { 1 }
-      expect(user).to receive(:password) { 'real' }
+      expect(user).to receive(:password) { 'secret' }
       expect(user).to receive(:confirmation) { true }
       expect(user).to receive(:authentity_token=) { token }
       expect(user).to receive(:save)
       expect(User).to receive(:find_by_email) { user }
       expect(SecureRandom).to receive(:hex).at_least(:once) { token }
 
-      post 'sessions/create', password: 'real', remember_me: true
+      post 'sessions/create', password: 'secret', remember_me: true
       expect(last_response).to be_redirect
       cookie = last_response['Set-Cookie']
       expect(cookie).to include('permanent_cookie')
       expect(cookie).to include('path=/')
       expect(cookie).to include('domain%3D%3E%22jobvacancy.de')
-      expect(cookie).to include('max-age=2592000')
+      expect(cookie).to include("max-age=#{thirty_days_in_seconds}")
     end
   end
 
