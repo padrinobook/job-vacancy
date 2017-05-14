3438845b8decadce7d86f4dd6ca8145a86ed720e
Matthias Guenther <matthias@wikimatze.de>
2017-05-14 10:47:59 +0200

    User class instead of string name for StringNormalizer

--- spec/lib/normalize_token_spec.rb
+++ spec/lib/normalize_token_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
-RSpec.describe "StringNormalizer" do
+RSpec.describe StringNormalizer do
   let(:string_normalizer) { Class.new { extend StringNormalizer } }
 
   subject { string_normalizer }
 
-  it "replaces slashes and + signs in strings" do
+  it 'replaces slashes and + signs in strings' do
     token = 'B4+K/32'
     expected_token = 'B4K32'
-expect(string_normalizer.normalize(token)).to eq expected_token
+    expect(subject.normalize(token)).to eq expected_token
   end
 end
+
