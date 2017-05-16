bb0c56a46b387d8af06bf715f772235803bfac35
Matthias Guenther <matthias@wikimatze.de>
2017-05-16 18:49:52 +0200

    Apply rubocop hints

--- app/mailers/confirmation.rb
+++ app/mailers/confirmation.rb
@@ -1,11 +1,11 @@
 JobVacancy::App.mailer :confirmation do
   # Find a better way to get into the main URL of the application ...
   # Padrino.root prints me out the path instead of the URL
-  CONFIRMATION_URL = "http://localhost:3000/confirm"
+  CONFIRMATION_URL = 'http://localhost:3000/confirm'.freeze
 
   email :confirmation_email do |name, email, id, link|
-    from "admin@job-vacancy.de"
-    subject "Please confirm your account"
+    from 'admin@job-vacancy.de'
+    subject 'Please confirm your account'
     to email
     locals :name => name, :confirmation_link => "#{CONFIRMATION_URL}/#{id}/#{link}"
     render 'confirmation/confirmation_email'
