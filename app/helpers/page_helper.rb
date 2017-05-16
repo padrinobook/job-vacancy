a48b0ee0230a3e26a412eb26c504356ca4434e33
Matthias Guenther <matthias@wikimatze.de>
2017-05-16 18:13:22 +0200

    Rewrite page-helper into new format

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -1,3 +1,12 @@
-JobVacancy::App.helpers do
+module JobVacancy
+  class App
+    module PageHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers PageHelper
+  end
 end
 
