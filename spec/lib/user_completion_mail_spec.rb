7bde97810ac7ee71b476f6712c339b6d1f161840
Matthias Guenther <matthias@wikimatze.de>
2017-08-27 10:17:47 +0200

    Use app of spec_helper

--- spec/lib/user_completion_mail_spec.rb
+++ spec/lib/user_completion_mail_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe UserCompletionMail do
       expected_confirmation_code = '$2a$10$y0Stx1HaYV.sZHuxYLb25.zAi0tu1C5N.oKMoPT6NbjtD.3cg7Au'
       expect(BCrypt::Engine).to receive(:generate_salt).and_return(salt)
       expect(BCrypt::Engine).to receive(:hash_secret).with(user.password, salt).and_return(expected_confirmation_code)
-      @user_completion_mail = UserCompletionMail.new(user, app(JobVacancy::App))
+      @user_completion_mail = UserCompletionMail.new(user, app)
       @user_completion_mail.encrypt_confirmation_code
 
       expect(@user_completion_mail.user.confirmation_code).to eq expected_confirmation_code
