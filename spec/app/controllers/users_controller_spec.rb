e2cff2da43eaa6ece66a131b8a2d4db3c4a3ea4d
Matthias Guenther <matthias@wikimatze.de>
2017-11-25 20:36:18 +0100

    Easier spec for GET confirm => saved user is also confirmed

--- spec/app/controllers/users_controller_spec.rb
+++ spec/app/controllers/users_controller_spec.rb
@@ -11,9 +11,7 @@ RSpec.describe "/users" do
   describe "GET confirm" do
     let(:user) { build(:user) }
     it "render the '/confirm' page if user has confirmation code" do
-      user.save
-      confirmed_user = User.find_by_id(user.id)
-      get "/confirm/#{confirmed_user.id}/#{confirmed_user.confirmation_token}"
+      get "/confirm/#{user.id}/#{user.confirmation_token}"
       expect(last_response).to be_ok
     end
 
