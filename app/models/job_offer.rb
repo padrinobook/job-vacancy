e9ced4a20dd54c44dc8c8ea8c68ac95446f1a7af
Matthias Guenther <matthias@wikimatze.de>
2019-01-15 21:33:48 +0100

    WIP of job_offer validation tests

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -1,4 +1,15 @@
 class JobOffer < ActiveRecord::Base
   belongs_to :user
+
+  validates :title, :description, :location, :contact, presence: true
+
+  validate :dates
+
+  private
+  def dates
+    if time_start.blank?
+      errors.add(:time_start, 'bla')
+    end
+  end
 end
 
--- spec/app/models/job_offer_spec.rb
+++ spec/app/models/job_offer_spec.rb
@@ -1,8 +1,46 @@
 require 'spec_helper'
 
 describe "JobOffer Model" do
-  let(:job_offer) { JobOffer.new }
+  let(:job_offer) { build_stubbed(:job_offer) }
+  let(:wrong_job_offer) { JobOffer.new }
+
   it 'can be created' do
     expect(job_offer).not_to be_nil
+  end
+
+  it 'must have a title' do
+    wrong_job_offer.title = ''
+    wrong_job_offer.description = 'hallo'
+
+    expect(wrong_job_offer.valid?).to be_falsey
+  end
+
+  it 'must have a description' do
+    wrong_job_offer.title = 'Hallo'
+    wrong_job_offer.description = ''
+
+    expect(wrong_job_offer.valid?).to be_falsey
+  end
+
+  it 'must have a location' do
+    wrong_job_offer.title = 'Hallo'
+    wrong_job_offer.description = 'hallo'
+    expect(wrong_job_offer.valid?).to be_falsey
+  end
+
+  it 'must have a contact' do
+    wrong_job_offer.title = 'Hallo'
+    wrong_job_offer.description = 'hallo'
+    wrong_job_offer.location = 'Berlin'
+    wrong_job_offer.contact = ''
+    expect(wrong_job_offer.valid?).to be_falsey
+  end
+
+  it 'must have a time_start' do
+    wrong_job_offer.title = 'Hallo'
+    wrong_job_offer.description = 'hallo'
+    wrong_job_offer.location = 'Berlin'
+    wrong_job_offer.contact = 'Test'
+    expect(wrong_job_offer.valid?).to be_falsey
   end
 end
