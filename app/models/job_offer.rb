b0c626cf0142963079578fec86dbc13138990cd8
Matthias Guenther <matthias@wikimatze.de>
2019-01-23 18:03:57 +0100

    Easier validation for dates

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -1,16 +1,18 @@
 class JobOffer < ActiveRecord::Base
   belongs_to :user
 
-  validates :title, :description, :location, :contact, presence: true
+  validates :title,
+    :description,
+    :location,
+    :contact,
+    :time_start,
+    :time_end,
+    presence: true
 
   validate :dates
 
   private
   def dates
-    if time_start.blank? || time_end.blank?
-      errors.add(:time_start, 'bla')
-    end
-
     if time_start && time_end && time_start > time_end
       errors.add(:time_end, 'must be after time_start')
     end
